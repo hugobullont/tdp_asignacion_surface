8dce8394997c779adff269ca7a20b165de49c100
hugobullont <hugobullont@gmail.com>
2018-08-30 23:50:54 -0500

    Added title and thumbnail url. Ready for implementation HTML.

--- Images_Videos/demoYouTube.js
+++ Images_Videos/demoYouTube.js
@@ -10,9 +10,18 @@ const youtube = google.youtube({
 async function runQuery () {
   const res = await youtube.search.list({
     part: 'id,snippet',
-    q: 'FIFA18'
+    q: 'FIFA19',
+    maxResults: 5
   });
-  console.log(res.data);
+
+  var videos = res.data.items;
+
+  for(var i = 0; i < videos.length; i++){
+    console.log(videos[i].snippet.title);
+    console.log(videos[i].snippet.thumbnails.high.url)
+  }
+
+
 }
 
 runQuery().catch(console.error);
