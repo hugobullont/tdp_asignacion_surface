63c5b90e002d7ba676721893f895a26e97a59055
hugobullont <hugobullont@gmail.com>
2018-08-31 12:06:01 -0500

    Added EJS and Express.

--- Images_Videos/demoYouTube.js
+++ Images_Videos/demoYouTube.js
@@ -1,17 +1,20 @@
 'use strict';
 
 const {google} = require('googleapis');
+const bodyParser = require('body-parser');
+const express = require('express');
 
+const app = express();
 const youtube = google.youtube({
   version: 'v3',
   auth: 'YourAPIKey'
 });
 
-async function runQuery () {
+async function runQuery (topic) {
   const res = await youtube.search.list({
     part: 'id,snippet',
-    q: 'FIFA19',
-    maxResults: 5
+    q: topic,
+    maxResults: 10
   });
 
   var videos = res.data.items;
@@ -24,4 +27,22 @@ async function runQuery () {
 
 }
 
-runQuery().catch(console.error);
+
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+})
+
+app.get('/', function (req, res) {
+  res.render('index');
+})
+
+app.post('/', function (req, res) {
+  res.render('index');
+  console.log(req.body.topic);
+  runQuery(req.body.topic).catch(console.error);
+})
