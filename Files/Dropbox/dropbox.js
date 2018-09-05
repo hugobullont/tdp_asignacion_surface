665b29df8b64496e638275d41024006fcbb2e820
hugobullont <hugobullont@gmail.com>
2018-09-04 21:26:40 -0500

    update dropbox.js

--- Files/Dropbox/dropbox.js
+++ Files/Dropbox/dropbox.js
@@ -1,16 +1,10 @@
 "use strict";
 
-<<<<<<< HEAD
 require('isomorphic-fetch');
 
 const bodyParser = require('body-parser');
 const express = require('express');
 const request = require('request');
-=======
-const bodyParser = require("body-parser");
-const express = require("express");
-const request = require("request");
->>>>>>> eabf3c329ac38da4cca5d75cf6e47b5f5953ec87
 
 const app = express();
 
@@ -40,7 +34,6 @@ app.post("/auth", function(req, res) {
   res.redirect(authUrl);
 });
 
-<<<<<<< HEAD
 app.get('/dropbox', function(req, res) {
     const code = req.query.code;
     console.log(code);
@@ -76,35 +69,3 @@ app.get('/dropbox', function(req, res) {
     );
 });
 
-=======
-app.get("/dropbox", function(req, res) {
-  const code = req.query.code;
-  console.log(code);
-  getDropboxToken(code);
-  dbx = new Dropbox({ accessToken: accesstoken });
-});
-
-function getDropboxToken(code) {
-  request.post(
-    "https://api.dropboxapi.com/oauth2/token",
-    {
-      json: true,
-      form: {
-        code: code,
-        grant_type: "authorization_code",
-        client_id: "s8ofpmn7nzknc7k",
-        client_secret: "qm9ihdekecjzztv",
-        redirect_uri: "http://localhost:3030/dropbox"
-      }
-    },
-    function(error, response, body) {
-      if (!error && response.statusCode == 200) {
-        accesstoken = body.access_token;
-        console.log(accesstoken);
-      } else {
-        console.log(error);
-      }
-    }
-  );
-}
->>>>>>> eabf3c329ac38da4cca5d75cf6e47b5f5953ec87
